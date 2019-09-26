710ee6a06c5e8bd3e46b0b45b70a8e0900cc1e06
chamikabm <chamika@bitsmedia.com>
2019-09-27 01:17:51 +0800

    First component screen created.

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
+
+// Method 1, Inline Styles.
+// const ComponentsScreen = () => {
+//
+//   return <Text style={{ fontSize: 30 }}>Components Screen!!</Text>;
+// };
+
+
+// Method 2.
 const ComponentsScreen = () => {
 
-  return <Text style={styles.textStyle}>Components Screen!!</Text>;
+  return <Text style={{ fontSize: 30 }}>Components Screen!!</Text>;
 };
 
-const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 30
-  }
-});
+// const styles = StyleSheet.create({
+//   textStyle: {
+//     fontSize: 30
+//   }
+// });
 
 export default ComponentsScreen;
