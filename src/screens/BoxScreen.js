a7b5a50fbd90aba82f1a0f528d369b25f10e96ba
chamikabm <chamika@bitsmedia.com>
2019-11-24 21:12:29 +0800

    Added changes for styling Box Screen View.

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,14 +4,22 @@ import { View, Text, StyleSheet } from 'react-native';
 const BoxScreen = () => {
 
   return (
-      <View>
-        <Text>Box Screen</Text>
+      <View style={styles.viewStyle}>
+        <Text style={styles.textStyle}>Box Screen</Text>
       </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  viewStyle: {
+    borderWidth: 2,
+    borderColor: 'black',
+  },
+  textStyle: {
+    borderWidth: 1,
+    borderColor: 'red',
+    margin: 10,
+  },
 });
 
 export default BoxScreen;
